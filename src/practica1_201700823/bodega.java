d3e98634cadab76912de4eb2d4f81cbe829a9f67
paula <paula@DESKTOP-LTB7J4Q>
2018-08-23 19:21:40 -0600

    medio avance

--- src/practica1_201700823/bodega.java
+++ src/practica1_201700823/bodega.java
@@ -5,10 +5,46 @@
  */
 package practica1_201700823;
 
+import java.util.Scanner;
+
 /**
  *
  * @author paula
  */
 public class bodega {
+ 
+   /* public void bodegon(){
+        Scanner S= new Scanner(System.in);
+    int[][]bodegas = new int[5][20];
+    for (int i = 0; i < bodegas.length; i++) {
+        System.out.println(";");
+            for (int j = 0; j < bodegas[0].length; j++) {
+                System.out.println("A= ["+i+"]["+j+"]");
+                bodegas[i][j]= S.nextInt();
+                       }
+                                 }
+        }*/
+
+  /* Scanner S= new Scanner(System.in);
+       public int[] b1 =new int[5];
+       public int[] b2 =new int[5];
+       public int[] b3 =new int[5];
+       public int[] b4 =new int[5];
+       public int[] b5 =new int[5];
+       public int[] b6 =new int[5];
     
-}
+       public void mostrar (){
+           int ensenar;
+           System.out.println("Ingrese el numero de bodega: ");
+           ensenar=S.nextInt();
+       switch (ensenar){
+           case 1:
+       
+       
+       }*/
+       
+       }
+    
+    }
+
+
